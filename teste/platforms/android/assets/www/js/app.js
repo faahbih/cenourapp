e121e4ef951be6700e6278bd23b6041a2b3a7615
Fabiana Oliveira <fabiana.rr.oliveira@gmail.com>
2017-08-17 19:39:02 -0300

    update config.xml

---
+++ teste/platforms/android/assets/www/js/app.js
@@ -0,0 +1,43 @@
+$('.collection-item').on('click', function(){
+
+	var $badge = $('.badge', this);
+	if ($badge.length == 0){
+		$badge = $('<span class="badge brown-text">0</span>').appendTo(this);
+	}
+
+	$badge.text(parseInt($badge.text()) + 1);
+
+
+});
+
+$('#confirmar').on('click', function(){
+
+
+	var texto = '';
+
+	$('.badge').parent().each(function(){
+		var produto = this.firstChild.textContent;
+		var quantidade = this.lastChild.textContent;
+
+		texto += produto + ': ' + quantidade + ', ';
+
+		// var nomeProduto = this.firstChild.textContent;
+		// Materialize.toast(nomeProduto + ' adicionado', 1000);
+	});
+
+	$('#resumo').text(texto);
+});
+
+$('.modal-trigger').leanModal();//inicializa plugin de modal do materialize
+
+
+$('.collection').on('click', '.badge', function(){
+	$(this).remove();
+	return false;
+
+});
+
+$('.acao-limpar').on('click', function() {
+	$('#numero-mesa').val('');
+	$('.badge').remove();
+})
